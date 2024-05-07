11f5bc2e5993cc0ab5a7c36fe22c11045b10ab93
ExLyD1 <sashaahnij@gmail.com>
2024-05-07 21:55:14 +0300

    qwe

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const butt = document.querySelector('.group')
-const adapt = document.querySelector('.adaptiveMenu')
-butt.addEventListener('click', ()=>{
-  adapt.classList.toggle('adaptive')
+// const butt = document.querySelector('.group')
+// const adapt = document.querySelector('.adaptiveMenu')
+// butt.addEventListener('click', ()=>{
+//   adapt.classList.toggle('adaptive')
+// })
+
+
+new Swiper('.swiper-container', {
+  slidesPerView:4,
+  spaceBetween: 20,
 })
+
